69e305e2708ad9e82247729fe6ae0e13d602145f
Kingsley <princekds.2006@gmail.com>
2025-12-04 13:28:50 +0000

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+//Modules - Encapsulated code
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavour')
+require('./7-mindgrenade')
+sayHi('susan');
+sayHi(names.john); 
+sayHi(names.peter);
