1de036bbcc97fb9bf373048279611f1c69762e43
Kingsley <princekds.2006@gmail.com>
2025-12-04 13:30:33 +0000

    first commit

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,6 +1,6 @@
 const {readFile, writeFile, read} = require('fs');
-console.log('start');
 
+console.log('start');
 readFile('./content/first.txt','utf8', (err,result)=>{
   if(err){
     console.log(err);
